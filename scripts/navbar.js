4d264fd5f37fd841a9f5f20cf9ebd748135e4e3e
itsmihirrao <39073078+itsmihirrao@users.noreply.github.com>
2021-02-03 23:12:29 -0800

    test branch
    add maintenance page

--- scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 10) {
-        $(".siteNavHeader").addClass("siteNavScrolled");
-    } else {
-        $(".siteNavHeader").removeClass("siteNavScrolled");
-    }
-});
